be77bbaaf4dbe821f09a70b879d9bbb6328925d4
Kitsune Ral <Kitsune-Ral@users.sf.net>
2016-09-15 12:27:22 +0900

    MessageEventModel: Provide the same default value for rowCount() argument as in the base class

--- client/models/messageeventmodel.h
+++ client/models/messageeventmodel.h
@@ -56,7 +56,7 @@ class MessageEventModel: public QAbstractListModel
 
         //override QModelIndex index(int row, int column, const QModelIndex& parent=QModelIndex()) const;
         //override QModelIndex parent(const QModelIndex& index) const;
-        int rowCount(const QModelIndex& parent) const override;
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
         QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
         QHash<int, QByteArray> roleNames() const override;
 
