f556a2231c33b53af4f5e007b1cbca6440ecf071
Malte Brandy <malte.brandy@maralorn.de>
2016-10-06 23:49:59 +0200

    Mark a room unread only if the new events are messages
    This changes three methods in QuaternionRoom.
    
    In setShown we now reset the readmarker whenever it is not at the
    bottom. And still delete the unread flag if it is set.
    
    In doAddNewMessageEvents we now set the room unread only if one of the new
    event really is a message.
    
    In processEphemeralEvents we now only act if the event is a receipt.
    If it is and there is no real message between the end and our own
    readmarker we delete the unread flag.
    If we can't find the readmarker we don't delete the unread flag, since
    we can't tell if the server has unread messages.

--- client/quaternionroom.cpp
+++ client/quaternionroom.cpp
@@ -43,16 +43,16 @@ void QuaternionRoom::setShown(bool shown)
     if( shown == m_shown )
         return;
     m_shown = shown;
-    if( m_shown && m_unreadMessages )
-    {
-        if( !messageEvents().empty() )
-            markMessageAsRead( messageEvents().last() );
-        m_unreadMessages = false;
-        emit unreadMessagesChanged(this);
-        qDebug() << displayName() << "no unread messages";
-    }
     if( m_shown )
     {
+        if ( !messageEvents().empty() && lastReadEvent(connection()->user()) != messageEvents().last()->id() )
+            markMessageAsRead( messageEvents().last() );
+        if( m_unreadMessages )
+        {
+            m_unreadMessages = false;
+            emit unreadMessagesChanged(this);
+            qDebug() << displayName() << "no unread messages";
+        }
         resetHighlightCount();
         resetNotificationCount();
     }
@@ -83,14 +83,19 @@ void QuaternionRoom::doAddNewMessageEvents(const QMatrixClient::Events& events)
     Room::doAddNewMessageEvents(events);
 
     m_messages.reserve(m_messages.size() + events.size());
+    bool new_message = false;
     for (auto e: events)
+    {
         m_messages.push_back(makeMessage(e));
+        if ( e->type() == QMatrixClient::EventType::RoomMessage )
+            new_message = true;
+    }
 
     if( m_shown )
     {
         markMessageAsRead(messageEvents().back());
     }
-    else if( !m_unreadMessages )
+    else if( !m_unreadMessages && new_message)
     {
         m_unreadMessages = true;
         emit unreadMessagesChanged(this);
@@ -110,13 +115,21 @@ void QuaternionRoom::doAddHistoricalMessageEvents(const QMatrixClient::Events& e
 void QuaternionRoom::processEphemeralEvent(QMatrixClient::Event* event)
 {
     QMatrixClient::Room::processEphemeralEvent(event);
-    QString lastReadId = lastReadEvent(connection()->user());
-    if( m_unreadMessages &&
-            (messageEvents().isEmpty() || lastReadId == messageEvents().last()->id()) )
+    if ( m_unreadMessages && event->type() == QMatrixClient::EventType::Receipt )
     {
-        m_unreadMessages = false;
-        emit unreadMessagesChanged(this);
-        qDebug() << displayName() << "no unread messages";
+        QString lastReadId = lastReadEvent(connection()->user());
+        for (int i = messageEvents().size()-1; i >= 0; i--)
+        {
+            if ( lastReadId == messageEvents().at(i)->id() )
+            {
+                m_unreadMessages = false;
+                emit unreadMessagesChanged(this);
+                qDebug() << displayName() << "no unread messages";
+                break;
+            }
+            if ( messageEvents().at(i)->type() == QMatrixClient::EventType::RoomMessage )
+                break;
+        }
     }
 }
 
