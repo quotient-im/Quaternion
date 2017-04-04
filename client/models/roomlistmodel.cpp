01b6d08543a9ea2b736e0aa081725301cf6addce
Elvis Angelaccio <elvis.angelaccio@kde.org>
2017-04-04 23:50:29 +0200

    Show the number of room participants in the room tooltip

--- client/models/roomlistmodel.cpp
+++ client/models/roomlistmodel.cpp
@@ -124,6 +124,7 @@ QVariant RoomListModel::data(const QModelIndex& index, int role) const
     if( role == Qt::ToolTipRole )
     {
         QString result = QString("<b>%1</b><br>").arg(room->canonicalAlias());
+        result += tr("Room users: %1<br>").arg(room->users().count());
         result += tr("Room ID: %1<br>").arg(room->id());
         if( room->joinState() == QMatrixClient::JoinState::Join )
             result += tr("You joined this room");
