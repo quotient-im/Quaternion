f75e0f289b4a22dc420d48b3bc904a0891f6f6c4
Elvis Angelaccio <elvis.angelaccio@kde.org>
2017-05-03 13:42:12 +0200

    Add support for flatpak
    Flatpak is a Linux tool that allows cross-distribution packages. This
    commit adds the JSON manifest file necessary to locally build a flatpak package.
    
    Qt is picked from the flatpak runtime provided by KDE. A couple of
    scripts contain the necessary flatpak commands to build and install the
    flatpak package.

---
+++ flatpak/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+flatpak-builder --ccache --force-clean --require-changes --repo=repo --subject="Nightly build of Quaternion, `date`" ${EXPORT_ARGS-} app com.github.quaternion.json
+
+flatpak --user remote-add --if-not-exists quaternion-nightly repo --no-gpg-verify
