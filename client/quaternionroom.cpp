e078d21d55c4a8b511bf66e53252c018bd1dac95
Ryan Andersen <ryco117@gmail.com>
2017-09-29 19:34:41 -0700

    Better design

--- client/quaternionroom.cpp
+++ client/quaternionroom.cpp
@@ -105,6 +105,8 @@ void QuaternionRoom::linkifyUrls(QString& text) const
 // (including TLD), with TLD having at least two characters and
 // starting with a letter (not a digit).
 #define FQDN "(\\w[-\\w]*\\.)+(?!\\d)\\w[-\\w]+"
+// https://stackoverflow.com/a/7109208
+#define VALID_URI "[A-Za-z0-9\\.\\/_~:%?#\\[\\]@!$&'\\(\\)*+,;=]*"
     static const QRegularExpression urlDetectorMail {
         QStringLiteral("(^|\\s)(" // Criteria to match the beginning of the URL
             "\\w[^@/#\\s]*@" // authentication (the part before @)
@@ -115,11 +117,11 @@ void QuaternionRoom::linkifyUrls(QString& text) const
     // Relative URLs require two dots in the series of characters. This is a bit
     // overly restrictive but eliminates dubious cases like command.com
     static const QRegularExpression urlDetectorRelative {
-        QStringLiteral("((^|\\s)(" "(\\w[-\\w]*\\.)" FQDN "\\b)"
-                       "|(&lt;)(" "(\\w[-\\w]*\\.)" FQDN "\\b)" "(&gt;))"), RegExpOptions
+        QStringLiteral("(^|\\s|&lt;(?=[^\\s]*&gt;))(" "(\\w[-\\w]*\\.)" FQDN "\\b)"), RegExpOptions
     };
     static const QRegularExpression urlDetectorAbsolute {
-        QStringLiteral("((^|\\s)(" // Criteria to match the beginning of the URL
+        QStringLiteral("(^|\\s)(" // Criteria to match the beginning of the URL
+                                                      // (and end of url if needs matching bracket)
             "(?!file)([a-z][-.+\\w]+:(//)?)" // scheme
             "(\\w[^@/#\\s]*@)?" // optional authentication (the part before @)
             "(" FQDN // host name
@@ -127,9 +129,15 @@ void QuaternionRoom::linkifyUrls(QString& text) const
                 "|\\[[\\d:a-f]+\\]" // or IPv6 address (very lazy and not strict)
             ")"
             "(:\\d{1,5})?" // optional port
-            "(/[^\\s]*)?" // optional query and fragment
-        "\\b)"
-        "|(&lt;)(" // Criteria to match '<' symbol in HTML plaintext
+            "(" VALID_URI ")?" // optional query and fragment
+        "\\b)"), // Criteria to match the end of the URL and '>' in HTML
+        RegExpOptions
+    };
+    // The absaolute paths need a separate regex for with angle brackets since the ending URI
+    // can contain the characters '&' 'g' 't' ';', but this string will be appended to URI
+    // when in HTML format
+    static const QRegularExpression urlDetectorAbsoluteWithAngleBrackets {
+        QStringLiteral("(&lt;)(" // Criteria to match the beginning '<'
             "(?!file)([a-z][-.+\\w]+:(//)?)" // scheme
             "(\\w[^@/#\\s]*@)?" // optional authentication (the part before @)
             "(" FQDN // host name
@@ -137,8 +145,8 @@ void QuaternionRoom::linkifyUrls(QString& text) const
                 "|\\[[\\d:a-f]+\\]" // or IPv6 address (very lazy and not strict)
             ")"
             "(:\\d{1,5})?" // optional port
-            "(/[^\\s]*)?" // optional query and fragment
-        "\\b)(&gt;))"), // Criteria to match the end of the URL and '>' in HTML
+            "(" VALID_URI ")?" // optional query and fragment
+        ")(&gt;)"), // Criteria to match the ending '>' in HTML
         RegExpOptions
     };
 #undef FQDN
@@ -147,9 +155,11 @@ void QuaternionRoom::linkifyUrls(QString& text) const
     text.replace(urlDetectorMail,
                  QStringLiteral("\\1<a href=\"mailto:\\2\">\\2</a>"));
     text.replace(urlDetectorRelative,
-                 QStringLiteral("\\2<a href=\"http://\\3\\7\">\\3\\6\\7\\10</a>"));
+                 QStringLiteral("\\1<a href=\"http://\\2\">\\2</a>"));
     text.replace(urlDetectorAbsolute,
-                 QStringLiteral("\\2<a href=\"\\3\\13\">\\3\\12\\13\\22</a>"));
+                 QStringLiteral("\\1<a href=\"\\2\">\\2</a>"));
+    text.replace(urlDetectorAbsoluteWithAngleBrackets,
+                 QStringLiteral("\\1<a href=\"\\2\">\\2</a>\\11"));
 }
 
 QString QuaternionRoom::prettyPrint(const QString& plainText) const
