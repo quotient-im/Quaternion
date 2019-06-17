quotient-im/Quaternion

commit 191d37dabc05b01617aecd88e1c88b4d021babeb

Support uploading attachments
Closes #209.

client/qml/ImageContent.qml
@@ -33,13 +33,13 @@ Attachment {
             cursorShape: Qt.PointingHandCursor
 
             onContainsMouseChanged:
-                controller.showStatusMessage(containsMouse ?
-                                                room.urlToDownload(eventId) : "")
-            onClicked: downloadAndOpen()
+                controller.showStatusMessage(containsMouse
+                                             ? room.fileSource(eventId) : "")
+            onClicked: openExternally()
         }
 
         Component.onCompleted:
-            if (visible && autoload && !downloaded)
+            if (visible && autoload && !(progressInfo && progressInfo.isUpload))
                 room.downloadFile(eventId)
     }
 
@@ -50,14 +50,14 @@ Attachment {
         spacing: 2
 
         Button {
-            text: qsTr("Cancel downloading")
-            visible: progressInfo.active && !downloaded
+            text: qsTr("Cancel")
+            visible: progressInfo.started
             onClicked: room.cancelFileTransfer(eventId)
         }
 
         Button {
             text: qsTr("Open externally")
-            onClicked: downloadAndOpen()
+            onClicked: openExternally()
         }
         Button {
             text: qsTr("Download full size")

client/chatroomwidget.cpp
@@ -23,6 +23,8 @@
 
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QToolButton>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QFileDialog>
 #include <QtWidgets/QApplication>
 
@@ -107,6 +109,32 @@ ChatRoomWidget::ChatRoomWidget(QWidget* parent)
     m_hudCaption = new QLabel();
     m_hudCaption->setWordWrap(true);
 
+    auto attachButton = new QToolButton();
+    attachButton->setAutoRaise(true);
+    m_attachAction = new QAction(QIcon::fromTheme("mail-attachment"),
+                                 tr("Attach"), attachButton);
+    m_attachAction->setCheckable(true);
+    m_attachAction->setDisabled(true);
+    connect(m_attachAction, &QAction::triggered, this, [this] (bool checked) {
+        if (checked)
+            attachedFileName =
+                    QFileDialog::getOpenFileName(this, tr("Attach file"));
+        else
+            attachedFileName.clear();
+
+        if (!attachedFileName.isEmpty())
+        {
+            m_chatEdit->setPlaceholderText(
+                tr("Add a message to the file or just push Enter"));
+            emit showStatusMessage(tr("Attaching ") + attachedFileName);
+        } else {
+            m_attachAction->setChecked(false);
+            m_chatEdit->setPlaceholderText(DefaultPlaceholderText);
+            emit showStatusMessage(tr("Attaching cancelled"), 3000);
+        }
+    });
+    attachButton->setDefaultAction(m_attachAction);
+
     m_chatEdit = new ChatEdit(this);
     m_chatEdit->setPlaceholderText(DefaultPlaceholderText);
     m_chatEdit->setAcceptRichText(false);
@@ -122,16 +150,21 @@ ChatRoomWidget::ChatRoomWidget(QWidget* parent)
             this, &ChatRoomWidget::typingChanged);
 
     auto layout = new QVBoxLayout();
-
-    auto headerLayout = new QHBoxLayout;
-    headerLayout->addWidget(m_roomAvatar);
-    headerLayout->addWidget(m_topicLabel, 1);
-    layout->addLayout(headerLayout);
-
+    {
+        auto headerLayout = new QHBoxLayout;
+        headerLayout->addWidget(m_roomAvatar);
+        headerLayout->addWidget(m_topicLabel, 1);
+        layout->addLayout(headerLayout);
+    }
     layout->addWidget(topicSeparator);
     layout->addWidget(qmlContainer);
     layout->addWidget(m_hudCaption);
-    layout->addWidget(m_chatEdit);
+    {
+        auto inputLayout = new QHBoxLayout;
+        inputLayout->addWidget(attachButton);
+        inputLayout->addWidget(m_chatEdit);
+        layout->addLayout(inputLayout);
+    }
     setLayout(layout);
 }
 
@@ -159,10 +192,13 @@ void ChatRoomWidget::setRoom(QuaternionRoom* room)
     readMarkerOnScreen = false;
     maybeReadTimer.stop();
     indicesOnScreen.clear();
+    attachedFileName.clear();
+    m_attachAction->setChecked(false);
     m_chatEdit->cancelCompletion();
 
     m_currentRoom = room;
     m_timelineWidget->rootContext()->setContextProperty("room", room);
+    m_attachAction->setEnabled(m_currentRoom != nullptr);
     if( m_currentRoom )
     {
         using namespace QMatrixClient;
@@ -294,7 +330,19 @@ QVector<QString> lazySplitRef(const QString& s, QChar sep, int maxParts)
 
 QString ChatRoomWidget::doSendInput()
 {
-    QString text = m_chatEdit->toPlainText();
+    auto text = m_chatEdit->toPlainText();
+    if (!attachedFileName.isEmpty())
+    {
+        Q_ASSERT(m_currentRoom != nullptr);
+        auto txnId = m_currentRoom->postFile(text,
+                        QUrl::fromLocalFile(attachedFileName));
+
+        attachedFileName.clear();
+        m_attachAction->setChecked(false);
+        m_chatEdit->setPlaceholderText(DefaultPlaceholderText);
+        return {};
+    }
+
     if ( text.isEmpty() )
         return tr("There's nothing to send");
 

client/chatroomwidget.h
@@ -39,6 +39,7 @@ class QQuickView;
 class QQuickWidget;
 #endif
 class QLabel;
+class QAction;
 class QTextDocument;
 
 class ChatRoomWidget: public QWidget
@@ -79,27 +80,32 @@ class ChatRoomWidget: public QWidget
         void setHudCaption(QString newCaption);
 
     private:
+        // Data
         MessageEventModel* m_messageModel;
         QuaternionRoom* m_currentRoom;
+        ImageProvider* m_imageProvider;
 
 #ifdef DISABLE_QQUICKWIDGET
         using timelineWidget_t = QQuickView;
 #else
         using timelineWidget_t = QQuickWidget;
 #endif
-        timelineWidget_t* m_timelineWidget;
-        ImageProvider* m_imageProvider;
-        ChatEdit* m_chatEdit;
-        QLabel* m_hudCaption; //< For typing and completion notifications
-        QLabel* m_topicLabel;
+        // Controls
         QLabel* m_roomAvatar;
+        QLabel* m_topicLabel;
+        timelineWidget_t* m_timelineWidget;
+        QLabel* m_hudCaption; //< For typing and completion notifications
+        QAction* m_attachAction;
+        ChatEdit* m_chatEdit;
 
+        // Supplementary/cache data members
         using timeline_index_t = QMatrixClient::TimelineItem::index_t;
         QVector<timeline_index_t> indicesOnScreen;
         timeline_index_t indexToMaybeRead;
         QBasicTimer maybeReadTimer;
         bool readMarkerOnScreen;
         QMap<QuaternionRoom*, QVector<QTextDocument*>> roomHistories;
+        QString attachedFileName;
 
         void reStartShownTimer();
         QString doSendInput();

client/qml/Attachment.qml
@@ -7,16 +7,16 @@ Item {
 
     property bool openOnFinished: false
     readonly property bool downloaded: progressInfo &&
-                progressInfo.downloading && progressInfo.completed
+                !progressInfo.isUpload && progressInfo.completed
 
     onDownloadedChanged: {
         if (downloaded && openOnFinished)
             openLocalFile()
     }
 
-    function downloadAndOpen()
+    function openExternally()
     {
-        if (downloaded)
+        if (progressInfo.localPath || downloaded)
             openLocalFile()
         else
         {

client/qml/FileContent.qml
@@ -34,17 +34,17 @@ Attachment {
             cursorShape: Qt.IBeamCursor
 
             onContainsMouseChanged:
-                controller.showStatusMessage(containsMouse ?
-                                                room.urlToDownload(eventId) : "")
+                controller.showStatusMessage(containsMouse
+                                             ? room.fileSource(eventId) : "")
         }
     }
     ProgressBar {
         id: transferProgress
-        visible: progressInfo.active && !progressInfo.completed
+        visible: progressInfo && progressInfo.started
         anchors.fill: fileTransferInfo
 
-        value: progressInfo.progress / progressInfo.total
-        indeterminate: progressInfo.progress < 0
+        value: progressInfo ? progressInfo.progress / progressInfo.total : -1
+        indeterminate: !progressInfo || progressInfo.progress < 0
     }
     RowLayout {
         anchors.top: fileTransferInfo.bottom
@@ -54,29 +54,30 @@ Attachment {
         CheckBox {
             id: openOnFinishedFlag
             text: qsTr("Open after downloading")
-            visible: progressInfo.downloading && transferProgress.visible
+            visible: progressInfo &&
+                     !progressInfo.isUpload && transferProgress.visible
             checked: openOnFinished
         }
         Button {
             text: qsTr("Cancel")
-            // Uploading has its own "Discard" button
-            visible: progressInfo.downloading && transferProgress.visible
+            visible: progressInfo && progressInfo.started
             onClicked: room.cancelFileTransfer(eventId)
         }
         Button {
             text: qsTr("Save as...")
-            visible: !transferProgress.visible
+            visible: !progressInfo ||
+                     (!progressInfo.isUpload && !progressInfo.started)
             onClicked: controller.saveFileAs(eventId)
         }
 
         Button {
             text: qsTr("Open")
             visible: !openOnFinishedFlag.visible
-            onClicked: downloadAndOpen()
+            onClicked: openExternally()
         }
         Button {
             text: qsTr("Open folder")
-            visible: progressInfo.active
+            visible: progressInfo && progressInfo.localDir
             onClicked:
                 Qt.openUrlExternally(progressInfo.localDir)
         }

client/qml/TimelineItem.qml
@@ -314,13 +314,17 @@ Item {
                 anchors.right: textField.right
 
                 sourceComponent: ImageContent {
-                    property var info: !progressInfo.active &&
-                        content.info && content.info.thumbnail_info ?
-                            content.info.thumbnail_info : content.info
+                    property var info:
+                        !progressInfo.uploading && !progressInfo.active &&
+                        content.info && content.info.thumbnail_info
+                        ? content.info.thumbnail_info
+                        : content.info
                     sourceSize: if (info) { Qt.size(info.w, info.h) }
-                    source: downloaded ? progressInfo.localPath :
-                            content.info && content.info.thumbnail_info ?
-                                "image://mtx/" + content.thumbnailMediaId : ""
+                    source: downloaded || progressInfo.uploading
+                            ? progressInfo.localPath
+                            : content.info && content.info.thumbnail_info
+                              ? "image://mtx/" + content.thumbnailMediaId
+                              : ""
                     maxHeight: chatView.height - textField.height -
                                authorLabel.height * !xchatStyle
                     autoload: settings.autoload_images

commit 30dbad171900d2560c38cd8cd13898a35e974b4a

Do not download already downloaded full-size images with autoload
It wastes bandwidth and causes flickering when changing rooms.

This got broken by 191d37d.

client/qml/ImageContent.qml
@@ -45,7 +45,7 @@ Attachment {
         }
 
         Component.onCompleted:
-            if (visible && autoload && !(progressInfo && progressInfo.isUpload))
+            if (visible && autoload && !downloaded && !(progressInfo && progressInfo.isUpload))
                 room.downloadFile(eventId)
     }
 