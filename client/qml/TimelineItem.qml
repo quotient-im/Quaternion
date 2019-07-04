quotient-im/Quaternion

commit dbda3932f303d8f1b0369028e4733508c5989e47

Timeline: scroll faster with mouse wheel
An easy-going vertical wheel scroll over the timeline is expected by
users even if shuttleDial is enabled.

client/qml/TimelineItem.qml
@@ -120,13 +120,17 @@ Item {
 
         Rectangle {
             width: parent.width
-            height: childrenRect.height + 2
+            height: sectionLabel.height + 2
             visible: sectionVisible
             color: defaultPalette.window
             Label {
+                id: sectionLabel
                 font.bold: true
                 renderType: settings.render_type
                 text: section
+            }
+            TimelineMouseArea {
+                anchors.fill: parent
             }
         }
         Loader {
@@ -138,12 +142,20 @@ Item {
             width: parent.width
 
             sourceComponent: detailsArea
+
+            TimelineMouseArea {
+                anchors.fill: parent
+            }
         }
 
         Item {
             id: message
             width: parent.width
             height: childrenRect.height
+
+            TimelineMouseArea {
+                anchors.fill: parent
+            }
 
             // There are several layout styles (av - author avatar,
             // al - author label, ts - timestamp, c - content

client/qml/Timeline.qml
@@ -262,6 +262,21 @@ Rectangle {
                 contentY = Math.min(originY + contentHeight - height,
                                     contentY + height)
             }
+            function onWheel(wheel) {
+                if (wheel.angleDelta.x == 0) {
+                    var yDelta = wheel.angleDelta.y / 120 * 100
+
+                    if (yDelta > 0) {
+                        contentY = Math.max(originY, contentY - yDelta)
+                    } else {
+                        contentY = Math.min(contentY + (originY + contentHeight) - (contentY + height),
+                                            contentY + Math.abs(yDelta))
+                    }
+                    wheel.accepted = true
+                } else {
+                    wheel.accepted = false
+                }
+            }
             Connections {
                 target: controller
                 onPageUpPressed: chatView.pageUp()

client/qml/TimelineMouseArea.qml
@@ -1,0 +1,5 @@
+import QtQuick 2.2
+
+MouseArea {
+    onWheel: chatView.onWheel(wheel)
+}

client/resources.qrc
@@ -14,5 +14,6 @@
         <file>qml/FileContent.qml</file>
         <file>qml/TimelineItem.qml</file>
         <file>qml/ActiveLabel.qml</file>
+        <file>qml/TimelineMouseArea.qml</file>
     </qresource>
 </RCC>

commit 5538a071a4925d8c0514fd94a25c769c10ada82d

Timeline: restore `Item#message` ability to shrink its height
This got broken by dbda393 which introduced circular dependency on
height: `Item#message` height was dependent on
`childrenRect.height` but it included a `MouseArea` which was
dependent on `parent.height`.

It is fixed by consolidation of three MouseAreas into one under the
top-level `Item`. This was suggested by Kitsune Ral.

client/qml/TimelineItem.qml
@@ -118,24 +118,24 @@ Item {
         }
     }
 
+    TimelineMouseArea {
+        anchors.fill: fullMessage
+        acceptedButtons: Qt.AllButtons
+    }
+
     Column {
         id: fullMessage
         width: parent.width
 
         Rectangle {
             width: parent.width
-            height: sectionLabel.height + 2
+            height: childrenRect.height + 2
             visible: sectionVisible
             color: defaultPalette.window
             Label {
-                id: sectionLabel
                 font.bold: true
                 renderType: settings.render_type
                 text: section
-            }
-            TimelineMouseArea {
-                anchors.fill: parent
-                acceptedButtons: Qt.AllButtons
             }
         }
         Loader {
@@ -147,22 +147,12 @@ Item {
             width: parent.width
 
             sourceComponent: detailsArea
-
-            TimelineMouseArea {
-                anchors.fill: parent
-                acceptedButtons: Qt.AllButtons
-            }
         }
 
         Item {
             id: message
             width: parent.width
             height: childrenRect.height
-
-            TimelineMouseArea {
-                anchors.fill: parent
-                acceptedButtons: Qt.AllButtons
-            }
 
             // There are several layout styles (av - author avatar,
             // al - author label, ts - timestamp, c - content