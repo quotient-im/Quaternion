quotient-im/Quaternion

commit 4fbcbb3e84f0e020e4c230b734a88391590509a9

Timeline: hand back the focus to the input line ASAP
There is no doubt that the most clicked widget is the timeline. We
click it to open pictures, to bring the window forward, to select
text, etc. Unfortunately, these actions ended up with unfocused
input line which was my primary reason to go ape on Quaternion.

This patch introduces a strict focus policy on the timeline which
means the timeline won't keep the focus for long, it will hand back
to the input line when possible, when pressed mouse buttons are
released.

At the same time we had to implement our own TextEdit text
selection mechanism because the original didn't pass through
required mouse events. Added bonus that deselection now works for
different messages (closes #300).

client/qml/TimelineItem.qml
@@ -131,6 +131,7 @@ Item {
             }
             TimelineMouseArea {
                 anchors.fill: parent
+                acceptedButtons: Qt.AllButtons
             }
         }
         Loader {
@@ -145,6 +146,7 @@ Item {
 
             TimelineMouseArea {
                 anchors.fill: parent
+                acceptedButtons: Qt.AllButtons
             }
         }
 
@@ -155,6 +157,7 @@ Item {
 
             TimelineMouseArea {
                 anchors.fill: parent
+                acceptedButtons: Qt.AllButtons
             }
 
             // There are several layout styles (av - author avatar,
@@ -203,7 +206,7 @@ Item {
 
                 text: (actionEvent ? "* " : "") + authorName
             }
-            MouseArea {
+            TimelineMouseArea {
                 anchors.left: authorAvatar.left
                 anchors.right: authorLabel.right
                 anchors.top: authorLabel.top
@@ -328,8 +331,11 @@ Item {
                         else
                             Qt.openUrlExternally(link)
                     }
+
+                    TimelineTextEditSelector {}
                 }
-                MouseArea {
+
+                TimelineMouseArea {
                     anchors.fill: parent
                     acceptedButtons: Qt.MiddleButton
 
@@ -340,14 +346,14 @@ Item {
                     }
                 }
 
-                MouseArea {
+                TimelineMouseArea {
                     anchors.fill: parent
                     acceptedButtons: Qt.RightButton
                     onClicked: controller.showMenu(index,
                         textFieldImpl.hoveredLink, showingDetails)
                 }
 
-                MouseArea {
+                TimelineMouseArea {
                     anchors.fill: parent
                     cursorShape: textFieldImpl.hoveredLink
                                  ? Qt.PointingHandCursor : Qt.IBeamCursor
@@ -492,6 +498,8 @@ Item {
                     anchors.left: parent.left
                     anchors.leftMargin: 3
                     z: 1
+
+                    TimelineTextEditSelector {}
                 }
                 TextEdit {
                     text: "<a href=\"" + evtLink + "\">"+ eventId
@@ -507,7 +515,9 @@ Item {
 
                     onLinkActivated: Qt.openUrlExternally(link)
 
-                    MouseArea {
+                    TimelineTextEditSelector {}
+
+                    TimelineMouseArea {
                         anchors.fill: parent
                         cursorShape: parent.hoveredLink ?
                                          Qt.PointingHandCursor :
@@ -536,6 +546,8 @@ Item {
 
                 width: parent.width
                 anchors.top: detailsHeader.bottom
+
+                TimelineTextEditSelector {}
             }
         }
     }

client/chatroomwidget.cpp
@@ -131,6 +131,11 @@ ChatRoomWidget::ChatRoomWidget(QWidget* parent)
     m_chatEdit->setAcceptRichText(false);
     m_chatEdit->setMaximumHeight(maximumChatEditHeight());
     connect( m_chatEdit, &KChatEdit::returnPressed, this, &ChatRoomWidget::sendInput );
+    connect(m_chatEdit, &KChatEdit::copyRequested, this, [=] {
+        QApplication::clipboard()->setText(
+            m_chatEdit->textCursor().hasSelection() ? m_chatEdit->textCursor().selectedText() : selectedText
+        );
+    });
     connect(m_chatEdit, &ChatEdit::proposedCompletion, this,
             [=](const QStringList& matches, int pos)
             {
@@ -725,6 +730,14 @@ void ChatRoomWidget::showMenu(int index, const QString& hoveredLink,
     menu.exec(QCursor::pos());
 }
 
+void ChatRoomWidget::setGlobalSelectionBuffer(QString text)
+{
+    if (QApplication::clipboard()->supportsSelection())
+        QApplication::clipboard()->setText(text, QClipboard::Selection);
+
+    selectedText = text;
+}
+
 void ChatRoomWidget::reStartShownTimer()
 {
     if (!readMarkerOnScreen || indicesOnScreen.empty() ||
@@ -811,4 +824,9 @@ void ChatRoomWidget::fileDrop(const QString& url)
 void ChatRoomWidget::textDrop(const QString& text)
 {
     m_chatEdit->setText(text);
+}
+
+Qt::KeyboardModifiers ChatRoomWidget::getModifierKeys()
+{
+    return QGuiApplication::keyboardModifiers();
 }

client/chatroomwidget.h
@@ -79,6 +79,8 @@ class ChatRoomWidget: public QWidget
         void showMenu(int index, const QString& hoveredLink, bool showingDetails);
         void fileDrop(const QString& url);
         void textDrop(const QString& text);
+        void setGlobalSelectionBuffer(QString text);
+        Qt::KeyboardModifiers getModifierKeys();
 
     private slots:
         void sendInput();
@@ -110,6 +112,7 @@ class ChatRoomWidget: public QWidget
         bool readMarkerOnScreen;
         QMap<QuaternionRoom*, QVector<QTextDocument*>> roomHistories;
         QString attachedFileName;
+        QString selectedText;
 
         void reStartShownTimer();
         QString doSendInput();

client/kchatedit.cpp
@@ -204,6 +204,11 @@ QSize KChatEdit::sizeHint() const
 
 void KChatEdit::keyPressEvent(QKeyEvent *event)
 {
+    if (event->matches(QKeySequence::Copy)) {
+        emit copyRequested();
+        return;
+    }
+
     switch (event->key()) {
     case Qt::Key_Enter:
     case Qt::Key_Return:

client/kchatedit.h
@@ -109,6 +109,11 @@ Q_SIGNALS:
      */
     void returnPressed();
 
+    /**
+     * Emitted when the user presses Ctrl+C.
+     */
+    void copyRequested();
+
 protected:
     void keyPressEvent(QKeyEvent *event) Q_DECL_OVERRIDE;
 

client/qml/ActiveLabel.qml
@@ -6,7 +6,7 @@ Label {
 
     font.italic: true
     textFormat: Text.PlainText
-    MouseArea
+    TimelineMouseArea
     {
         anchors.fill: parent
         cursorShape: Qt.PointingHandCursor

client/qml/FileContent.qml
@@ -27,7 +27,7 @@ Attachment {
         textFormat: TextEdit.PlainText
         wrapMode: Text.Wrap;
 
-        MouseArea {
+        TimelineMouseArea {
             anchors.fill: parent
             acceptedButtons: Qt.NoButton
             hoverEnabled: true
@@ -37,7 +37,7 @@ Attachment {
                                              ? room.fileSource(eventId) : "")
         }
 
-        MouseArea {
+        TimelineMouseArea {
             anchors.fill: parent
             acceptedButtons: Qt.RightButton
             cursorShape: Qt.IBeamCursor

client/qml/ImageContent.qml
@@ -25,7 +25,7 @@ Attachment {
 //            easing.type: Easing.OutQuad
 //        }}
 
-        MouseArea {
+        TimelineMouseArea {
             anchors.fill: parent
             acceptedButtons: Qt.LeftButton
             hoverEnabled: true
@@ -36,7 +36,7 @@ Attachment {
             onClicked: openExternally()
         }
 
-        MouseArea {
+        TimelineMouseArea {
             anchors.fill: parent
             acceptedButtons: Qt.RightButton
             cursorShape: Qt.PointingHandCursor

commit 66b4680ba7471a3ab3564c0ff36b0346f762fdb7

TimelineItem: fix unpressable scrollbar in the details pane
We don't care about lost input focus when interacting with the
details pane anymore because it is a rare event. This careless
attitude is the best fix for the unpressable scrollbar there.

Got broken by 4fbcbb3e, closes #619

client/qml/TimelineItem.qml
@@ -515,8 +515,6 @@ Item {
                     anchors.left: parent.left
                     anchors.leftMargin: 3
                     z: 1
-
-                    TimelineTextEditSelector {}
                 }
                 TextEdit {
                     text: "<a href=\"" + evtLink + "\">"+ eventId
@@ -532,9 +530,7 @@ Item {
 
                     onLinkActivated: Qt.openUrlExternally(link)
 
-                    TimelineTextEditSelector {}
-
-                    TimelineMouseArea {
+                    MouseArea {
                         anchors.fill: parent
                         cursorShape: parent.hoveredLink ?
                                          Qt.PointingHandCursor :
@@ -563,8 +559,6 @@ Item {
 
                 width: parent.width
                 anchors.top: detailsHeader.bottom
-
-                TimelineTextEditSelector {}
             }
         }
     }