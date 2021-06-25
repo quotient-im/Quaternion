29d02c2c6c6c299546ba5191ccd22f13fe522f8c
Alexey Rusakov <Kitsune-Ral@users.sf.net>
2021-06-25 14:22:53 +0200

    0.0.95-rc

--- client/main.cpp
+++ client/main.cpp
@@ -72,7 +72,7 @@ int main( int argc, char* argv[] )
     QApplication::setOrganizationName(QStringLiteral("Quotient"));
     QApplication::setApplicationName(QStringLiteral("quaternion"));
     QApplication::setApplicationDisplayName(QStringLiteral("Quaternion"));
-    QApplication::setApplicationVersion(QStringLiteral("0.0.95-beta5+git"));
+    QApplication::setApplicationVersion(QStringLiteral("0.0.95-rc"));
     QApplication::setDesktopFileName(
         QStringLiteral("com.github.quaternion.desktop"));
 
