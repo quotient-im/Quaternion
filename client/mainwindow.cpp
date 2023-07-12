quotient-im/Quaternion

commit dab20d8df8cfe2790cc3bb7ee670ee6ba67f3508

Only add accounts to the registry after successful login
Working around https://github.com/quotient-im/libQuotient/issues/670.

client/mainwindow.cpp
@@ -526,6 +526,7 @@ void MainWindow::addConnection(Connection* c)
 
     using Room = Quotient::Room;
 
+    accountRegistry->add(c);
     c->loadState();
     c->setLazyLoading(true);
 
@@ -723,7 +724,6 @@ void MainWindow::doOpenLoginDialog(LoginDialog* dialog)
         account.sync();
         dialog->deleteLater();
 
-        accountRegistry->add(connection);
         addConnection(connection);
         showInitialLoadIndicator();
     });
@@ -901,7 +901,6 @@ void MainWindow::invokeLogin()
                     else
                         ci->onNetworkError(error);
                 });
-        accountRegistry->add(c);
         if (legacyLocation) {
             connect(c, &Connection::connected, this, [this, c] {
                 qInfo()

commit a09ef51e1f7ca5a676e516143208d5b9db45f62c

Don't show LoginDialog if some accounts are loading
This is a regression from dab20d8d - won't be needed any more once
https://github.com/quotient-im/libQuotient/issues/670 is fixed.

client/mainwindow.cpp
@@ -879,6 +879,7 @@ inline QString accessTokenKey(const KeySourceT& source, bool legacyLocation)
 void MainWindow::invokeLogin()
 {
     const auto accounts = Quotient::SettingsGroup("Accounts").childGroups();
+    bool showLoginDialog = true;
     for (const auto& accountId: accounts) {
         Quotient::AccountSettings account { accountId };
 
@@ -889,6 +890,7 @@ void MainWindow::invokeLogin()
         if (token.isEmpty()) // The account is saved but not logged-in
             continue;
 
+        showLoginDialog = false;
         qDebug().noquote().nospace()
             << "Found an access token for " << account.userId() << '/'
             << account.deviceId() << ", trying to connect";
@@ -922,7 +924,7 @@ void MainWindow::invokeLogin()
     }
     // By now, either no accounts were found or whichever were found are
     // retrieving their access tokens (or resolving their homeservers at least)
-    if (accountRegistry->empty())
+    if (showLoginDialog)
         showLoginWindow(tr("Welcome to Quaternion"));
     else
         showInitialLoadIndicator();